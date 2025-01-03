2c0c74a2f529eb2150fb1d61e6d20d978a122dbb
Natália Sampaio Rosa e Silva <natalia.srs@outlook.com>
2025-01-03 18:37:13 -0300

    refactor due to API update

--- js/script.js
+++ js/script.js
@@ -1,23 +1,47 @@
 import { renderMovie } from "./renderMovie.js";
-import { apiKey } from "./apiKey.js";
 
 async function getPopularMovies() {
-    const url = `https://api.themoviedb.org/3/movie/popular?api_key=${apiKey}&language=pt-BR&page=1`;
-    const fetchResponse = await fetch(url);
-    const response = await fetchResponse.json();
-    return response.results.forEach(movie => renderMovie(movie));
+    const options = {
+        method: "GET",
+        headers: {
+            accept: "application/json",
+            Authorization:
+                "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI3ZjdhNWI5MmZmMTFmOTAwZTVkM2ZmZjBjZDM1YTE2MCIsIm5iZiI6MTY3NjQxMTAwNC4zMzQsInN1YiI6IjYzZWMwMDdjNjk5ZmI3MDA5NmFkNTQ3OSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.A1jA04-hYun37uPOWf1rM2LcMhnKbLoO4jXG21zgfUs",
+        },
+    };
+
+    fetch(
+        "https://api.themoviedb.org/3/movie/popular?language=pt-BR&page=1",
+        options
+    )
+        .then((res) => res.json())
+        .then((res) => res.results.forEach((movie) => renderMovie(movie)))
+        .catch((err) => console.error(err));
 }
 
 getPopularMovies();
 
 async function searchMovie(search_title) {
-    const url = `https://api.themoviedb.org/3/search/movie?api_key=06a89df5363d1e40a48fe8fe3503438b&language=en-US&query=${search_title}&page=1&include_adult=false`
-    const fetchResponse = await fetch(url);
-    const response = await fetchResponse.json();
+    const options = {
+        method: "GET",
+        headers: {
+            accept: "application/json",
+            Authorization:
+                "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI3ZjdhNWI5MmZmMTFmOTAwZTVkM2ZmZjBjZDM1YTE2MCIsIm5iZiI6MTY3NjQxMTAwNC4zMzQsInN1YiI6IjYzZWMwMDdjNjk5ZmI3MDA5NmFkNTQ3OSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.A1jA04-hYun37uPOWf1rM2LcMhnKbLoO4jXG21zgfUs",
+        },
+    };
+
     if (search_title === "") {
         location.reload();
     }
-    return response.results.forEach(movie => renderMovie(movie));
+
+    fetch(
+        `https://api.themoviedb.org/3/search/movie?query=${search_title}&include_adult=false&language=pt-BR&page=1`,
+        options
+    )
+        .then((res) => res.json())
+        .then((res) => res.results.forEach((movie) => renderMovie(movie)))
+        .catch((err) => console.error(err));
 }
 
 function clearPage() {
@@ -38,10 +62,12 @@ search_title.addEventListener("keypress", function (event) {
 });
 
 function showFavoritedMovies() {
-    JSON.parse(localStorage.getItem('movies')).forEach(movie => renderMovie(movie));
+    JSON.parse(localStorage.getItem("movies")).forEach((movie) =>
+        renderMovie(movie)
+    );
 }
 
-var filterFavoriteMovies = document.querySelector('#filtro');
+var filterFavoriteMovies = document.querySelector("#filtro");
 filterFavoriteMovies.addEventListener("change", function () {
     if (this.checked) {
         clearPage();
